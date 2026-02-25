aa2e03ad4186e9df97975b2705eb0e68abd15479
Jay <herforth.jason@gmail.com>
2026-02-25 11:20:20 -0500

    Updated smart_doors.lua

--- villagers/systems/smart_doors.lua
+++ villagers/systems/smart_doors.lua
@@ -3,62 +3,26 @@
 
 lualore.smart_doors = {}
 
-local DAY_START   = 0.25    -- 6AM
-local DAY_END     = 0.9167  -- 10PM
-local CHECK_INTERVAL = 10   -- Globalstep poll interval in seconds
+local DAY_START      = 0.25    -- 6AM
+local DAY_END        = 0.9167  -- 10PM
+local CHECK_INTERVAL = 10      -- Globalstep poll interval in seconds
 
 --------------------------------------------------------------------
 -- HELPER FUNCTIONS
 --------------------------------------------------------------------
-
-local function is_closed_door(node_name)
-	return node_name and (
-		node_name:match("^doors:door_.*_b$") or
-		node_name:match("^doors:hidden$")
-	)
-end
-
-local function is_open_door(node_name)
-	return node_name and node_name:match("^doors:door_.*_a$")
-end
-
-local function get_open_door_name(closed_name)
-	return closed_name:gsub("_b$", "_a")
-end
-
-local function get_closed_door_name(open_name)
-	return open_name:gsub("_a$", "_b")
-end
 
 local function is_daytime()
 	local tod = minetest.get_timeofday()
 	return tod >= DAY_START and tod < DAY_END
 end
 
-local function open_door(pos, node)
-	local open_name = get_open_door_name(node.name)
-	if minetest.registered_nodes[open_name] then
-		minetest.swap_node(pos, {name = open_name, param1 = node.param1, param2 = node.param2})
-		local def = minetest.registered_nodes[open_name]
-		if def and def.sound_open then
-			minetest.sound_play(def.sound_open, {pos = pos, gain = 0.3, max_hear_distance = 10})
-		end
-		return true
-	end
-	return false
+local function is_door_open(pos)
+	local state = minetest.get_meta(pos):get_int("state")
+	return state % 2 == 1
 end
 
-local function close_door(pos, node)
-	local closed_name = get_closed_door_name(node.name)
-	if minetest.registered_nodes[closed_name] then
-		minetest.swap_node(pos, {name = closed_name, param1 = node.param1, param2 = node.param2})
-		local def = minetest.registered_nodes[closed_name]
-		if def and def.sound_close then
-			minetest.sound_play(def.sound_close, {pos = pos, gain = 0.3, max_hear_distance = 10})
-		end
-		return true
-	end
-	return false
+local function is_registered_door(node_name)
+	return doors and doors.registered_doors and doors.registered_doors[node_name]
 end
 
 --------------------------------------------------------------------
@@ -74,27 +38,18 @@ local function update_doors_near_players(want_open)
 		local minp = vector.subtract(ppos, 128)
 		local maxp = vector.add(ppos, 128)
 
-		if want_open then
-			local positions = minetest.find_nodes_in_area(minp, maxp, {"group:door"})
-			for _, pos in ipairs(positions) do
-				local key = minetest.pos_to_string(pos)
-				if not visited[key] then
-					visited[key] = true
-					local node = minetest.get_node(pos)
-					if is_closed_door(node.name) then
-						open_door(pos, node)
-					end
-				end
-			end
-		else
-			local positions = minetest.find_nodes_in_area(minp, maxp, {"group:door"})
-			for _, pos in ipairs(positions) do
-				local key = minetest.pos_to_string(pos)
-				if not visited[key] then
-					visited[key] = true
-					local node = minetest.get_node(pos)
-					if is_open_door(node.name) then
-						close_door(pos, node)
+		local positions = minetest.find_nodes_in_area(minp, maxp, {"group:door"})
+		for _, pos in ipairs(positions) do
+			local key = minetest.pos_to_string(pos)
+			if not visited[key] then
+				visited[key] = true
+				local node = minetest.get_node(pos)
+				if is_registered_door(node.name) then
+					local currently_open = is_door_open(pos)
+					if want_open and not currently_open then
+						doors.door_toggle(pos, node, nil)
+					elseif not want_open and currently_open then
+						doors.door_toggle(pos, node, nil)
 					end
 				end
 			end
@@ -107,7 +62,7 @@ end
 --------------------------------------------------------------------
 
 local elapsed_accum = 0
-local last_was_day = nil
+local last_was_day  = nil
 
 minetest.register_globalstep(function(dtime)
 	elapsed_accum = elapsed_accum + dtime
@@ -117,7 +72,6 @@ minetest.register_globalstep(function(dtime)
 	local now_day = is_daytime()
 
 	if last_was_day == nil then
-		-- First tick: bring all doors to the correct state without firing transitions
 		update_doors_near_players(now_day)
 		last_was_day = now_day
 		return
